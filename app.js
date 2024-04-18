666856a6dce698237c2435e4ffbc8518c59d25dc
Sujan Thakare <sujan.thakare@pocketlaw.com>
2024-04-18 22:15:13 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require("node:http");
+
+// Create a simple HTTP server that returns "Hello, World!"
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+});
+
+// Listen on port 3000
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server running at http://127.0.0.1:3000/");
+});
